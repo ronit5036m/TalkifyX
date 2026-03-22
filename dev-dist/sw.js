103ceaf361ad19dd9da6b6480eff8225ffe83587
ronit5036m <ronitshakhari2005@gmail.com>
2026-03-22 09:32:11 +0530

    sync-last-commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h3059t43u68"
+    "revision": "0.btod6o05qg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
